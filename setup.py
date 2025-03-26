7a29e770c7029c24d89879c8621544035dce9d99
Giuliano Difranco <127220297+GiulianoWasHere@users.noreply.github.com>
2025-03-26 17:36:51 +0100

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qubitcooling",
-    version="0.777",
+    version="1",
     author="GiulianoWasHere",
     short_description="QuL is a python package to generate, analyze and test quantum circuits for various computational cooling protocols.",
     long_description="QuL is a python package to generate, analyze and test quantum circuits for various computational cooling protocols.",
